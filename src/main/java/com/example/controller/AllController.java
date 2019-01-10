7606872756e9cbf8da586c2f2b6e4ed48984d3d1
ismi <ismi@DESKTOP-36FB4V3>
2019-01-10 17:14:45 +0800

    first commit

---
+++ src/main/java/com/example/controller/AllController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AllController {
+
+	
+	@RequestMapping("/ss")//登入
+	public String login() {
+		return "index.html";
+	}
+	
+}
